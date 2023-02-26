3eee3c3119b217369dd19e6dacde1d2bfa4e4a93
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-02-26 16:56:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function countWords() {
 			let text = document.getElementById("evaluatedText").value;
 			let wordCount = 0;
 			if (text.trim() !== "") {
+				// regx '\s+' will split at one or more spaces
+				// trim() will remove trailing and leading spaces
 				wordCount = text.trim().split(/\s+/).length;
 			}
 			document.getElementById("wordCount").textContent = wordCount;
