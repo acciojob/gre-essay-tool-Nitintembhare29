6172c848d1e029390db9c0cd3e98a87b4e5f6a08
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-02-26 16:41:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+function countWords() {
+			let text = document.getElementById("evaluatedText").value;
+			let wordCount = 0;
+			if (text.trim() !== "") {
+				wordCount = text.trim().split(/\s+/).length;
+			}
+			document.getElementById("wordCount").innerHTML = wordCount;
+		}
